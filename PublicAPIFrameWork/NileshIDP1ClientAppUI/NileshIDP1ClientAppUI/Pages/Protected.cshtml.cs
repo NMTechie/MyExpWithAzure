7a86e2f10b31a230589742f50efdbfd2dac48936
Nilesh Mitra <jmitrani@jci.com>
2022-09-26 21:14:47 +0530

    sets up the basics of protected resource

---
+++ PublicAPIFrameWork/NileshIDP1ClientAppUI/NileshIDP1ClientAppUI/Pages/Protected.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NileshIDP1ClientAppUI.Pages
+{
+    public class ProtectedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
